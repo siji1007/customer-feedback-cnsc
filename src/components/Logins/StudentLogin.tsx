a1e5f5f4d100a45b799844d764e1393e594f1520
siji1007 <christianjohnibanez@gmail.com>
2024-07-19 14:02:52 +0800

    resolve

--- src/components/Logins/StudentLogin.tsx
+++ src/components/Logins/StudentLogin.tsx
@@ -57,6 +57,7 @@ const StudentLogin: React.FC<StudentLoginProps> = ({ onLoginSuccess }) => {
     event.preventDefault();
     try {
       onLoginSuccess();
+
       const response = await axios.post(
         import.meta.env.VITE_APP_SERVERHOST + "student-login",
         formData,
