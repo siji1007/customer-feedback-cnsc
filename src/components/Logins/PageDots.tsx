4273d680bc9fec74ef119c9edbc345a4c6c2adfe
siji1007 <christianjohnibanez@gmail.com>
2024-09-04 01:21:30 +0800

    update pageDot

--- src/components/Logins/PageDots.tsx
+++ src/components/Logins/PageDots.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { CheckIcon } from "@heroicons/react/24/solid";
 
 interface PageDotsProps {
   totalDots: number;
@@ -7,35 +8,30 @@ interface PageDotsProps {
 
 const PageDots: React.FC<PageDotsProps> = ({ totalDots, completedDots }) => {
   return (
-    <div className="flex space-x-2">
-      {Array.from({ length: totalDots }).map((_, index) => {
-        const isCompleted = index < completedDots;
-        return (
-          <div
-            key={index}
-            className={`w-4 h-4 rounded-full flex items-center justify-center ${
-              isCompleted ? "bg-black" : "bg-gray-400"
-            }`}
-          >
-            {isCompleted && (
-              <svg
-                className="w-3 h-3 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 13l4 4L19 7"
-                />
-              </svg>
+    <div className="flex items-center justify-center space-x-4">
+      {Array.from({ length: totalDots }).map((_, index) => (
+        <React.Fragment key={index}>
+          <div className="relative">
+            <div
+              className={`w-4 h-4 rounded-full flex items-center justify-center ${
+                index < completedDots ? "bg-red-800" : "bg-gray-300"
+              }`}
+            >
+              {index < completedDots && (
+                <CheckIcon className="w-3 h-3 text-white" />
+              )}
+            </div>
+            {/* Add the connector line */}
+            {index < totalDots - 1 && (
+              <div
+                className={`absolute left-full top-1/2 transform -translate-y-1/2 w-8 h-1 ${
+                  index < completedDots - 1 ? "bg-red-800" : "bg-gray-300"
+                }`}
+              />
             )}
           </div>
-        );
-      })}
+        </React.Fragment>
+      ))}
     </div>
   );
 };
